e488b4b4910a99cbd27d234954ecea4293cfa089
Jessica Almand <jlalmand27@gmail.com>
2022-12-04 13:59:05 -0500

    Added more to CSS

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,7 +3,7 @@ import '../styles/app.css'
 
 const About = () => {
     return (
-        <div style={{width: '18rem', class:'card', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div class='about' style={{width: '18rem', class:'card', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <h1 style={{class: 'card-title'}}>About Me</h1>
         <p class="card-body">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
