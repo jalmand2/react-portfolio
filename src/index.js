65cb695f9722f35bfd533bc0c157c280bbfd7904
Jessica Almand <jlalmand27@gmail.com>
2022-12-04 13:44:51 -0500

    Renamed css and relinked

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/styles/header.css'
+import './components/styles/app.css'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
