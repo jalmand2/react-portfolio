ae8d3820ec8145abd807015d6ed101185a0c044d
Jessica Almand <jlalmand27@gmail.com>
2022-12-07 02:13:22 -0500

    Readjusted image size to match in portfolio

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -4,15 +4,16 @@ const Portfolio = () => {
     return (
         <div class="container row container-fluid" style={{margin: 'auto'}}>
         <div class="card" style={{width: '18rem'}}>
-        <div class="card-img-top" src=".../100px180/" alt="Card image cap"></div>
+        <img class="card-img-top" src="/images/waves.png" alt="Waves over Work"></img>
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="https://github.com/Tab-Y/anime-tracker" class="btn btn-secondary">Github</a>
+          <h5 class="card-title text-center">Waves over Work</h5>
+          <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="https://github.com/MariaYConstanza/beach-day" class="btn btn-secondary">Github</a>
+          <a href="https://mariayconstanza.github.io/beach-day/" class="btn btn-secondary">Link</a>
         </div>
       </div>
       <div class="card" style={{width: '18rem'}}>
-        <img class="card-img-top" src="/images/animetracker.png" alt="Card image cap"></img>
+        <img class="card-img-top" src="/images/animetracker.png" alt="Anime Tracker"></img>
         <div class="card-body">
           <h5 class="card-title text-center">Anime Tracker</h5>
           <p class="card-text text-center">This was a collaborative, fullstack project. </p>
