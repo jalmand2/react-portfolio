88e614b5e727ed400952eda0a74274bab9ebcfc1
Jessica Almand <jlalmand27@gmail.com>
2022-12-07 22:03:43 -0500

    Finished adding cards

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Portfolio = () => {
     return (
         <div class="container row container-fluid projectBg " style={{margin: 'auto', alignContent: 'flex-start', justifyContent:'center'}}>
-        <div class="card" style={{width: '18rem'}}>
+        <div class="card" style={{width: '18rem', backgroundColor: '#7c90dbff'}}>
         <img class="card-img-top" src="/images/waves.png" alt="Waves over Work"></img>
         <div class="card-body">
           <h5 class="card-title text-center">Waves over Work</h5>
@@ -14,7 +14,7 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-      <div class="card" style={{width: '18rem'}}>
+      <div class="card" style={{width: '18rem', backgroundColor: '#7c90dbff'}}>
         <img class="card-img-top" src="/images/animetracker.png" alt="Anime Tracker"></img>
         <div class="card-body">
           <h5 class="card-title text-center">Anime Tracker</h5>
@@ -25,7 +25,7 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-      <div class="card" style={{width: '18rem'}}>
+      <div class="card" style={{width: '18rem', backgroundColor: '#7c90dbff'}}>
         <img class="card-img-top" src="/images/notetaker.png" alt="Note Taker"></img>
         <div class="card-body">
           <h5 class="card-title text-center">Note Taker</h5>
@@ -36,7 +36,7 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-      <div class="card" style={{width: '18rem'}}>
+      <div class="card" style={{width: '18rem', backgroundColor: '#7c90dbff'}}>
         <img class="card-img-top" src="/images/workscheduler.jpg" alt="Work Scheduler"></img>
         <div class="card-body">
           <h5 class="card-title text-center">Work Scheduler</h5>
@@ -47,23 +47,26 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-      <div class="card" style={{width: '18rem'}}>
-        <div class="card-img-top" src="/images/workscheduler.jpeg" alt="Work Scheduler"></div>
+      <div class="card" style={{width: '18rem', backgroundColor: '#7c90dbff'}}>
+        <img class="card-img-top" src="/images/weatherdashboard.png" alt="Weather Dashboard"></img>
         <div class="card-body">
-          <h5 class="card-title text-center">App</h5>
-          <p class="card-text text-center">A simple calendar application featuring HTML and CSS powered by jQuery.</p>
+          <h5 class="card-title text-center">Weather Dashboard</h5>
+          <p class="card-text text-center">An application using the Open Weather API.</p>
           <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
-          <a href="https://github.com/Tab-Y/anime-tracker" class="btn btn-secondary">Github</a>
-          <a href="https://github.com/Tab-Y/anime-tracker" class="btn btn-secondary">Link</a>
+          <a href="https://github.com/jalmand2/Weather-Dashboard-App" class="btn btn-secondary">Github</a>
+          <a href="https://jalmand2.github.io/Weather-Dashboard-App/" class="btn btn-secondary">Link</a>
           </div>
         </div>
       </div>
-      <div class="card" style={{width: '18rem'}}>
-        <div class="card-img-top" src=".../100px180/" alt="Card image cap"></div>
+      <div class="card" style={{width: '18rem', backgroundColor: '#7c90dbff'}}>
+        <img class="card-img-top" src="/images/codingquiz.jpg" alt="Coding Quiz"></img>
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="https://github.com/Tab-Y/anime-tracker" class="btn btn-secondary">Github</a>
+          <h5 class="card-title text-center">Coding Quiz</h5>
+          <p class="card-text text-center">A quiz application using HTML, CSS, and JavaScript.</p>
+          <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+          <a href="https://github.com/jalmand2/Coding-Quiz" class="btn btn-secondary">Github</a>
+          <a href="https://jalmand2.github.io/Coding-Quiz/" class="btn btn-secondary">Link</a>
+          </div>
         </div>
       </div>
       </div>
